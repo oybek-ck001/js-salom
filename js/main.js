6087b3fbf1f5d66af06e2f7f9aaba99df134b325
oybek <oybekjorayev500@gmail.com>
2022-07-29 17:39:13 +0500

    dars

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var dars = prompt("ismingizni kriting")
+var yosh = prompt("yilingizni kiriting")
+var bolu = (2022 - yosh)
+function ismi(ism = "kimdur") {
+    return("salom " + ism);
+}
+console.log(ismi(dars + " yoshingiz " + bolu + " da"));
